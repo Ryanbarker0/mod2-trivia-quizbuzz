d109cf95451da5e70b8aab84ad6aaf4fbe731b53
Ryan <ryangeorgebarker0@gmail.com>
2018-10-23 23:05:25 +0100

    style

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   root 'application#home'
 
+  get '/register', to: 'users#new', as: 'register'
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
