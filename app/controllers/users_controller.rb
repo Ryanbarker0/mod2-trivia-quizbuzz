256933dc21ea7ad6788c69eea40f918e2abc22ec
Ryan <ryangeorgebarker0@gmail.com>
2018-10-23 09:00:40 +0100

    form template, user creation, validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,21 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      session[:user_id] = @user.id
+      redirect_to categories_url
+    else
+      flash[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
   private
 
   def set_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   has_many :categories, through: :games
   has_secure_password
 
+  validates :name, length: {minimum: 3}
+  validates_uniqueness_of :name
+  validates :password, length: {minimum: 5}
+
+
 end
